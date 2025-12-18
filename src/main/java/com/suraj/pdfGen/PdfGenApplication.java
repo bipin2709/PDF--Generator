9638e5d33faf4e3750a0fe3b1a6d378d62a99d03
bipin2709 <bipinyadav2709@gmail.com>
2025-12-18 12:59:45 +0530

    first commit

--- src/main/java/com/suraj/pdfGen/PdfGenApplication.java
+++ src/main/java/com/suraj/pdfGen/PdfGenApplication.java
@@ -1,5 +1,6 @@
 package com.suraj.pdfGen;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,5 @@ public class PdfGenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PdfGenApplication.class, args);
 	}
-
+//
 }
